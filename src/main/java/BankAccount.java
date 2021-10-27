dad76a6b889b8ae604626b8a8cad929d176e2d75
parkerschwantes <23pschwantes@athenian.org>
2021-10-27 10:34:02 -0700

    done

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,6 +1,58 @@
 public class BankAccount {
     // Instance variables
+
     private int balance;
     private int accountNumber;
     private String preferredName;
+
+    public BankAccount(int bal, int num, String pref){
+        balance = bal;
+        accountNumber = num;
+        preferredName = pref;
+    }
+
+    public void withdraw(int x){
+        if(balance > x){
+            balance = balance - x;
+            System.out.println("Your new balance is $" + balance);
+        }
+        else{
+            System.out.println("You don't have enough money for that transaction");
+        }
+    }
+
+    public void deposit(int y){
+        balance = balance + y;
+        System.out.println("Your new balance is $" + balance);
+
+    }
+
+    public int getBalance(){
+
+        return balance;
+    }
+
+    public int getAccountNumber(){
+
+        return accountNumber;
+    }
+
+    public String getPreferredName(){
+
+        return preferredName;
+    }
+
+    public void setPreferredName(String name2){
+
+        preferredName = name2;
+    }
+
+    public String toString(String b){
+        b = "Your bank account number is " + accountNumber + " and you balance is $" + balance;
+
+        return b;
+    }
+
+
+
 }
--- src/main/java/BankClient.java
+++ src/main/java/BankClient.java
@@ -8,5 +8,13 @@ public class BankClient {
         String name = scan.nextLine();
 //        // Create a bank account with a balance of 0, the account number 101134, and the given name
 //        BankAccount account = new BankAccount(0, 101134, name);
+        BankAccount account = new BankAccount(123789, 90123 ,name);
+        account.deposit(100);
+        System.out.println(account.getAccountNumber());
+        System.out.println(account.getPreferredName());
+        // System.out.println(account.setPreferredName("Daquavis"));
+        account.toString();
+
+
     }
 }
